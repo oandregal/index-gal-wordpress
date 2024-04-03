46657a498c122d0da219f0b7abebf4f3ae686ae1
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-04-03 14:18:39 +0200

    Minimal DataViews

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { DataViews, filterSortAndPaginate } from '@wordpress/dataviews';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState( [
+		{
+			id: 1,
+			title: 'First',
+			description: 'Description for first item.',
+			author: 'Author',
+			date: "2012-01-23T18:25:43.511Z",
+		},
+		{
+			id: 2,
+			title: 'Second',
+			description: 'Description for second item.',
+			author: 'Editor',
+			date: "2012-02-23T18:25:43.511Z",
+		},
+		{
+			id: 3,
+			title: 'Third',
+			description: 'Description for third item.',
+			author: 'Admin',
+			date: "2012-03-23T18:25:43.511Z",
+		},
+	] );
+	const [paginationInfo, setPaginationInfo] = useState({
+		totalItems: 0,
+		totalPages: 0,
+	});
+	const [view, setView] = useState({
+		type: 'table',
+		hiddenFields: [],
+		layout: {},
+		filters: [],
+	});
+	const fields = [
+		{
+			id: 'title',
+			header: 'Title'
+		},
+		{
+			id: 'description',
+			header: 'Description'
+		},
+		{
+			id: 'author',
+			header: 'Author'
+		},
+		{
+			id: 'date',
+			header: 'Date'
+		}
+	];
+
+	const onChangeView = (view) => {
+		const { data: newData, paginationInfo: newPaginationInfo } = filterSortAndPaginate(data, view, fields);
+		setView(view);
+		setData( newData );
+		setPaginationInfo( newPaginationInfo );
+	}
+
+	return (
+		<DataViews data={data} view={view} fields={fields} paginationInfo={paginationInfo} onChangeView={onChangeView} />
+	);
 }
 
 export default App;
