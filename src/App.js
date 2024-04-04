db096f072d5ca3849ef24d2602c5ee480c783685
André Maneiro <583546+oandregal@users.noreply.github.com>
2024-04-04 14:33:54 +0200

    Styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import { DataViews, filterSortAndPaginate } from '@wordpress/dataviews';
 import {useState} from 'react';
 
+import '@wordpress/dataviews/build-style/style.css';
+import '@wordpress/components/build-style/style.css';
+
 function App() {
 	const [data, setData] = useState( [
 		{
 			id: 1,
 			title: 'First',
-			description: 'Description for first item.',
+			description: 'Description for the first item.',
 			author: 'Author',
 			date: "2012-01-23T18:25:43.511Z",
 		},
 		{
 			id: 2,
 			title: 'Second',
-			description: 'Description for second item.',
+			description: 'Description for the second item.',
 			author: 'Editor',
 			date: "2012-02-23T18:25:43.511Z",
 		},
 		{
 			id: 3,
 			title: 'Third',
-			description: 'Description for third item.',
+			description: 'Description for the third item.',
 			author: 'Admin',
 			date: "2012-03-23T18:25:43.511Z",
+		},
+		{
+			id: 4,
+			title: 'Fourth',
+			description: 'Description for the fourth item.',
+			author: 'Author',
+			date: "2012-04-23T18:25:43.511Z",
+		},
+		{
+			id: 5,
+			title: 'Fifth',
+			description: 'Description for the fifth item.',
+			author: 'Editor',
+			date: "2012-05-23T18:25:43.511Z",
+		},
+		{
+			id: 6,
+			title: 'Sixth',
+			description: 'Description for the sixth item.',
+			author: 'Admin',
+			date: "2012-06-23T18:25:43.511Z",
 		},
 	] );
 	const [paginationInfo, setPaginationInfo] = useState({
